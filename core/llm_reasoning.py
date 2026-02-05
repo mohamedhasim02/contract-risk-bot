601a55ab80448fbd6da8c7be6e7bce3798c15fb6
Mohamed Hasim <mohamedhasim2002@gmail.com>
2026-02-05 20:38:43 +0530

    Use environment variable for OpenAI key

--- core/llm_reasoning.py
+++ core/llm_reasoning.py
@@ -1,6 +1,9 @@
 
 import openai
-openai.api_key="YOUR_API_KEY"
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 def explain_clause(clause):
     prompt=f"Explain this clause simply and suggest safer wording:\n{clause}"
     r=openai.ChatCompletion.create(
