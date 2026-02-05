edcf5c2efaf73a221965f683197f995d659d8f71
Mohamed Hasim <147535511+mohamedhasim02@users.noreply.github.com>
2026-02-05 21:44:25 +0530

    Update report_generator.py

--- core/report_generator.py
+++ core/report_generator.py
@@ -1,8 +1,15 @@
-
 from fpdf import FPDF
+import os
+
 def generate_pdf(summary, filename):
-    pdf=FPDF()
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+    pdf = FPDF()
     pdf.add_page()
+    pdf.set_auto_page_break(auto=True, margin=15)
     pdf.set_font("Arial", size=10)
-    pdf.multi_cell(0,8,summary)
+
+    for line in summary.split("\n"):
+        pdf.multi_cell(0, 8, line)
+
     pdf.output(filename)
